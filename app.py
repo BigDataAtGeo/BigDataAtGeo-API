39e202a54bc7fdb32afce7f09f92c86a3c19ea1c
Michael Steininger <steininger@informatik.uni-wuerzburg.de>
2019-10-29 12:02:58 +0100

    Implemented Index route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # import argparse
 import os
+import glob
 import time
 import pandas as pd
 from geojson import GeometryCollection, Feature, FeatureCollection, Point  # Polygon
@@ -27,30 +28,18 @@ app = Flask(__name__)
 app.config.from_mapping(config)
 cache = Cache(app)
 Compress(app)
-CORS(app)  # , resources={r"/*": {"origins": "*"}})
+CORS(app)
+
+var_dict = {
+    'pr': 'Mittlerer Jahresniederschlag',
+    'tas': 'Mittlere Jahrestemperatur'
+}
 
 
 def build_feature(row: pd.Series, data_col: str, prop_name: str = 'temperature') -> Feature:
-    # print('BUILD FEATURES')
-    # t0 = time.time()
     loc = (row['lon'], row['lat'])
-    # t1 = time.time()
-    # print('resolve loc -', t1-t0)
-    # Find column that is not 'lat' or 'lon', since this one contains the data
-    # data_col = [c for c in row.index if c not in ['lat', 'lon']][0]
-    # t2 = time.time()
-    # print('find col -', t2-t1)
     geom = Point((loc[0], loc[1]))
-    # geom = Polygon([[(loc[0] - 0.00005, loc[1] - 0.00005),
-    #                  (loc[0] + 0.00005, loc[1] - 0.00005),
-    #                  (loc[0] + 0.00005, loc[1] + 0.00005),
-    #                  (loc[0] - 0.00005, loc[1] + 0.00005),
-    #                  (loc[0] - 0.00005, loc[1] - 0.00005)]])
-    # t2 = time.time()
-    # print('Build polygon -', t2-t1)
-    f = Feature(geometry=geom, properties={prop_name: row[data_col]})
-    # t3 = time.time()
-    # print('Build feature -', t3-t2)
+    f = Feature(geometry=geom, properties={'value': row[data_col]})
     return f
 
 
@@ -66,6 +55,50 @@ def all_locations(scenario: str, var: str, timerange: str) -> Response:
     key = DictKey(var=var, scenario=scenario)
     data = csv_dict[key]
     return pandas_to_geojson(data[['lon', 'lat', timerange]], data_col=timerange, prop_name=var)
+
+
+@app.route('/index', methods=['GET'])
+@cache.cached()
+def index() -> str:
+    files = glob.glob(os.path.join(path, '*.txt'))
+
+    # We assume that each file has the same timeranges for now
+    file_headers = open(files[0]).readline().rstrip()
+    timeranges = [h.strip() for h in file_headers.split(',')[2:-1]]
+
+    files = [f.split(os.path.sep)[-1].split('_') for f in files]
+    # Find all unique scenarios
+    scenarios = list(set([f[4] for f in files]))
+    # Find all unique variables
+    variables = list(set([f[0] for f in files]))
+    variables = [{'var_id': v, 'var': var_dict[v]} for v in variables]
+
+    file_infos = {
+                    'variables': variables,
+                    'scenarios': scenarios,
+                    'timeranges': timeranges
+                  }
+
+    return jsonify(file_infos)
+
+# @app.route('/index', methods=['GET'])
+# @cache.cached()
+# def index() -> str:
+#     files = glob.glob(os.path.join(path, '*.txt'))
+
+#     file_headers = [open(f).readline().rstrip() for f in files]
+#     files = [f.split(os.path.sep)[-1].split('_') for f in files]
+
+#     files = zip(files, file_headers)
+
+#     file_infos = [{
+#                     'var_id': f[0],
+#                     'var': var_dict[f[0]],
+#                     'scenario': f[4],
+#                     'timeranges': [h.strip() for h in h.split(',')[2:-1]]
+#                   } for f, h in files]
+
+#     return jsonify(file_infos)
 
 # @app.route('/time', methods=['GET'])
 # def time() -> Response:
