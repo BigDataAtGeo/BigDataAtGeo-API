3fdae4c37c3039cf06c9417e22b1816cc5f458c2
Michael Steininger <steininger@informatik.uni-wuerzburg.de>
2020-01-15 11:56:20 +0100

    It now works with more files

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 import os
 import glob
 import time
+from collections import OrderedDict
 from sqlalchemy import create_engine, Table, MetaData, func
 import pandas as pd
 from geojson import GeometryCollection, Feature, FeatureCollection, Point  # Polygon
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, Response, jsonify, abort
 from flask_caching import Cache
 from flask_compress import Compress
 from flask_cors import CORS
+from tqdm import tqdm
 
 # parser = argparse.ArgumentParser()
 # parser.add_argument('-p', '--path', type=str, help='Path to folder with CSV files to serve.')
@@ -29,34 +31,100 @@ CORS(app)
 
 engine = create_engine('sqlite://', echo=False)
 
-var_dict = {
-    'pr': {
+var_dict = OrderedDict([
+    ('pr-year', {
         'var': 'Mittlerer Jahresniederschlag',
         'unit': 'mm/qm',
         'description': 'Absolute Niederschlagssumme (Regen und Schnee) in mm',
         'colormap': 'YlGnBu',
         'min': float('inf'),
         'max': -float('inf')
-    },
-    'tas': {
+    }),
+    ('pr-djf', {
+        'var': 'Mittlerer Niederschlag (Winter)',
+        'unit': 'mm/qm',
+        'description': 'Absolute Niederschlagssumme (Regen und Schnee) in mm zwischen Dezember und Februar',
+        'colormap': 'YlGnBu',
+        'min': float('inf'),
+        'max': -float('inf')
+    }),
+    ('pr-mam', {
+        'var': 'Mittlerer Niederschlag (Frühjahr)',
+        'unit': 'mm/qm',
+        'description': 'Absolute Niederschlagssumme (Regen und Schnee) in mm zwischen März und Mai',
+        'colormap': 'YlGnBu',
+        'min': float('inf'),
+        'max': -float('inf')
+    }),
+    ('pr-jja', {
+        'var': 'Mittlerer Niederschlag (Sommer)',
+        'unit': 'mm/qm',
+        'description': 'Absolute Niederschlagssumme (Regen und Schnee) in mm zwischen Juni und August',
+        'colormap': 'YlGnBu',
+        'min': float('inf'),
+        'max': -float('inf')
+    }),
+    ('pr-son', {
+        'var': 'Mittlerer Niederschlag (Herbst)',
+        'unit': 'mm/qm',
+        'description': 'Absolute Niederschlagssumme (Regen und Schnee) in mm zwischen September und November',
+        'colormap': 'YlGnBu',
+        'min': float('inf'),
+        'max': -float('inf')
+    }),
+    ('tas-year', {
         'var': 'Mittlere Jahrestemperatur',
         'unit': '°C',
         'description': 'Mittlere Lufttemperatur (in 2 Meter Höhe) in °C',
-        'colormap': 'Warm',
+        'colormap': 'Turbo',
         'min': float('inf'),
         'max': -float('inf')
-    },
-    'vp_vernal': {
+    }),
+    ('tas-djf', {
+        'var': 'Mittlere Temperatur (Winter)',
+        'unit': '°C',
+        'description': 'Mittlere Lufttemperatur (in 2 Meter Höhe) in °C zwischen Dezember und Februar',
+        'colormap': 'Turbo',
+        'min': float('inf'),
+        'max': -float('inf')
+    }),
+    ('tas-mam', {
+        'var': 'Mittlere Temperatur (Frühjahr)',
+        'unit': '°C',
+        'description': 'Mittlere Lufttemperatur (in 2 Meter Höhe) in °C zwischen März und Mai',
+        'colormap': 'Turbo',
+        'min': float('inf'),
+        'max': -float('inf')
+    }),
+    ('tas-jja', {
+        'var': 'Mittlere Temperatur (Sommer)',
+        'unit': '°C',
+        'description': 'Mittlere Lufttemperatur (in 2 Meter Höhe) in °C zwischen Juni und August',
+        'colormap': 'Turbo',
+        'min': float('inf'),
+        'max': -float('inf')
+    }),
+    ('tas-son', {
+        'var': 'Mittlere Temperatur (Herbst)',
+        'unit': '°C',
+        'description': 'Mittlere Lufttemperatur (in 2 Meter Höhe) in °C zwischen September und November',
+        'colormap': 'Turbo',
+        'min': float('inf'),
+        'max': -float('inf')
+    }),
+    ('vp_vernal-year', {
         'var': 'Thermische Vegetationsperiode',
         'unit': 'Tage',
         'description': """ Die thermische Vegetationsperiode eines Jahres ist definiert als die Anzahl Tage zwischen
+<br>
 1.) Vegetationsbeginn:  Erstes Aufkommen von mindestens 6 aufeinanderfolgenden Tagen mit einer Durchschnittstemperatur über 5°C und
+<br>
 2.) Vegetationsende:    Erstes Aufkommen von mindestens 6 aufeinanderfolgenden Tagen mit einer Durchschnittstemperatur unter 5°C im Winterhalbjahr. """,
         'colormap': 'Warm',
         'min': float('inf'),
         'max': -float('inf')
-    },
-    'huglin': {
+    }),
+    ('huglin-year', {
         'var': 'Huglin Index',
         'unit': '',
         'description': """ Ein für den Weinbau konzipierter Wärmesummenindex aus Tagesmittel- und Maximumtemperaturen im Sommerhalbjahr (April-September).
@@ -73,31 +141,31 @@ var_dict = {
 <td>keine Anbauempfehlung</td>
 </tr>
 <tr>
-<td style="background: #d7fefe;">1500 ≤ H &lt; 1600</td>
+<td>1500 ≤ H &lt; 1600</td>
 <td><a href="https://de.wikipedia.org/wiki/M%C3%BCller_Thurgau">Müller Thurgau</a></td>
 </tr>
 <tr>
-<td style="background: #fffc6b;">1600 ≤ H &lt; 1700</td>
+<td>1600 ≤ H &lt; 1700</td>
 <td><a href="https://de.wikipedia.org/wiki/Pinot_Blanc">Pinot Blanc</a> ,  <a href="https://de.wikipedia.org/wiki/Grauer_Burgunder">Grauer Burgunder</a> ,  <a href="https://de.wikipedia.org/wiki/Aligot%C3%A9">Aligoté</a> ,  <a href="https://de.wikipedia.org/wiki/Gamay">Gamay</a>  Noir,  <a href="https://de.wikipedia.org/wiki/Gew%C3%BCrztraminer">Gewürztraminer</a></td>
 </tr>
 <tr>
-<td style="background: #f7c444;">1700 ≤ H &lt; 1800</td>
+<td>1700 ≤ H &lt; 1800</td>
 <td><a href="https://de.wikipedia.org/wiki/Riesling">Riesling</a> ,  <a href="https://de.wikipedia.org/wiki/Chardonnay">Chardonnay</a> ,  <a href="https://de.wikipedia.org/wiki/Silvaner">Silvaner</a> ,  <a href="https://de.wikipedia.org/wiki/Sauvignon_Blanc">Sauvignon Blanc</a> ,  <a href="https://de.wikipedia.org/wiki/Pinot_Noir">Pinot Noir</a> ,  <a href="https://de.wikipedia.org/wiki/Gr%C3%BCner_Veltliner">Grüner Veltliner</a></td>
 </tr>
 <tr>
-<td style="background: #ed5629;">1800 ≤ H &lt; 1900</td>
+<td>1800 ≤ H &lt; 1900</td>
 <td><a href="https://de.wikipedia.org/wiki/Cabernet_Franc">Cabernet Franc</a></td>
 </tr>
 <tr>
-<td style="background: #eb3223; color: white;">1900 ≤ H &lt; 2000</td>
+<td>1900 ≤ H &lt; 2000</td>
 <td><a href="https://de.wikipedia.org/wiki/Chenin_Blanc">Chenin Blanc</a> ,  <a href="https://de.wikipedia.org/wiki/Cabernet_Sauvignon">Cabernet Sauvignon</a> ,  <a href="https://de.wikipedia.org/wiki/Merlot">Merlot</a> ,  <a href="https://de.wikipedia.org/wiki/Semillion">Semillion</a> ,  <a href="https://de.wikipedia.org/wiki/Welschriesling">Welschriesling</a></td>
 </tr>
 <tr>
-<td style="background: #911b12; color: white;">2000 ≤ H &lt; 2100</td>
+<td>2000 ≤ H &lt; 2100</td>
 <td><a href="https://de.wikipedia.org/wiki/Ugni_Blanc">Ugni Blanc</a></td>
 </tr>
 <tr>
-<td  style="background: #4f0a05; color: white;">2100 ≤ H &lt; 2200</td>
+<td>2100 ≤ H &lt; 2200</td>
 <td><a href="https://de.wikipedia.org/wiki/Grenache">Grenache</a> ,  <a href="https://de.wikipedia.org/wiki/Syrah">Syrah</a> ,  <a href="https://de.wikipedia.org/wiki/Cinsaut">Cinsaut</a></td>
 </tr>
 <tr>
@@ -111,25 +179,24 @@ var_dict = {
 </tbody>
 </table>
 
-Quelle für Tabelle:  [Stock, M. et al. (2007) Perspektiven der Klimaänderung bis 2050 für den Weinbau in Deutschland (Klima 2050). Schlußbericht zum FDW-Vorhaben Klima 2050. PIK Report 106.](https://www.pik-potsdam.de/research/publications/pikreports/.files/pr106.pdf)  
-Daten für Karten: MPI-ESM-REMO RCP8.5' """
+Quelle für Tabelle:  <a href="https://www.pik-potsdam.de/research/publications/pikreports/.files/pr106.pdf">Stock, M. et al. (2007) Perspektiven der Klimaänderung bis 2050 für den Weinbau in Deutschland (Klima 2050). Schlußbericht zum FDW-Vorhaben Klima 2050. PIK Report 106.</a> """
         ,
         'colormap': 'Warm',
         'min': float('inf'),
         'max': -float('inf')
-    },
-    'tmin_lt_0': {
+    }),
+    ('tmin_lt_0-year', {
         'var': 'Frosttage',
         'unit': 'Tage',
         'description': 'Anzahl der Tage, an denen die minimale Lufttemperatur unter 0°C sinkt',
         'colormap': 'Warm',
         'min': float('inf'),
         'max': -float('inf')
-    }
-}
+    })
+])
 
 scenarios = set()
-variables = set()
+variables = var_dict.keys()  # set()
 timeranges = set()
 
 
@@ -138,16 +205,29 @@ def init() -> None:
     files = glob.glob(os.path.join(path, '*.txt'))
 
     # Load all files
-    for f in files:
+    for f in tqdm(files):
         parts = f.split(os.path.sep)[-1].split('_')
         scenario = parts[4]
         variable = parts[0]
-        timeframe = parts[-2]  # year, djf, jja, mam, son
-        aggregation = parts[-1]  # mean, min, max
+        timeframe = 'year'
+        aggregation = parts[-1].split('.')[0]  # mean, min, max
 
         # Ignore aggregations other than mean for now
         if aggregation != 'mean':
             continue
+
+        # The file names are insane. We have to do weird shit here. Sorry.
+        if 'huglin' in f:
+            variable = 'huglin'
+        elif 'vp_vernal' in f:
+            variable = 'vp_vernal'
+        elif 'tmin_lt_0' in f:
+            variable = 'tmin_lt_0'
+        else:
+            # This is for tas and pr where we have data for seasons
+            timeframe = parts[-2]  # year, djf, jja, mam, son
+
+        variable = variable + '-' + timeframe
 
         data = pd.read_csv(f, skipinitialspace=True).dropna(axis=1)
         # Store the row number as the id for each cell/row
@@ -160,7 +240,7 @@ def init() -> None:
         data['scenario'] = scenario
 
         scenarios.add(scenario)
-        variables.add(variable)
+        # variables.add(variable)
         timeranges.update(list(data['timerange']))
 
         # Calculate Min/Max values for each variable
@@ -196,20 +276,30 @@ def pandas_to_geojson(data: pd.DataFrame, data_col: str, prop_name: str = 'tempe
 @app.route('/all_locations/<scenario>/<var>/<timerange>', methods=['GET'])
 @cache.cached()
 def all_locations(scenario: str, var: str, timerange: str) -> Response:
+
+    if scenario not in scenarios or var not in variables or timerange not in timeranges:
+        abort(404)
+
     data = pd.read_sql(data_table.select().where(data_table.c.var == var)
                                           .where(data_table.c.scenario == scenario)
                                           .where(data_table.c.timerange == timerange), con=engine)
-
     return pandas_to_geojson(data, data_col='value', prop_name=var)
 
 
 @app.route('/all_times/<int:cell_id>/<scenario>/<var>', methods=['GET'])
 @cache.cached()
 def all_times(cell_id: int, scenario: str, var: str) -> Response:
+
+    if scenario not in scenarios or var not in variables:
+        abort(404)
+
     data = pd.read_sql(data_table.select().where(data_table.c.var == var)
                                           .where(data_table.c.scenario == scenario)
                                           .where(data_table.c.id == cell_id)
                                           .order_by(data_table.c.timerange), con=engine)
+
+    if data.shape[0] == 0:
+        abort(404)
 
     row = data.iloc[0]
 
