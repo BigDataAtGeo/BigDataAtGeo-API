712331cb3d183f8b21b2ad9ab6803379303d4bc6
Michael Steininger <steininger@informatik.uni-wuerzburg.de>
2021-03-29 12:00:11 +0200

    v3.1.0

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,2 @@
-docker build -t gitlab2.informatik.uni-wuerzburg.de:4567/dmir/bigdata-at-geo/bdatgeo-rest-api:3.0.0 .
-docker push gitlab2.informatik.uni-wuerzburg.de:4567/dmir/bigdata-at-geo/bdatgeo-rest-api:3.0.0
+docker build -t gitlab2.informatik.uni-wuerzburg.de:4567/dmir/bigdata-at-geo/bdatgeo-rest-api:3.1.0 .
+docker push gitlab2.informatik.uni-wuerzburg.de:4567/dmir/bigdata-at-geo/bdatgeo-rest-api:3.1.0
